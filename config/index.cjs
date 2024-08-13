6210e17a2f04af894e0474094f78879192505624
jack-nack <109973638+lcx777@users.noreply.github.com>
2024-08-13 19:38:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -44,7 +44,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oboOi6YVE7fL2bERhOaqSw-zBEwM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Zbp3DlZTIKTh4SkDacKl673WtBw_68AfyNUauJ7Oc1U',
+      useTemplateId: 'DXZ7be3Z2zjUnXjkc2gv-y9vbU_Rxl0hTTOgcZuZJIQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-21',
       festivals: [
