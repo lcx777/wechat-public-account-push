eec630bdd6d06334cadabce77f4e269cb748cb05
jack-nack <109973638+lcx777@users.noreply.github.com>
2024-08-13 16:10:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -16,6 +16,27 @@ const USER_CONFIG = {
   PROVINCE: '贵州省黔西南布依族苗族自治州',
   CITY: '望谟',
 
+  TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: 'd2ae8479c91c1c7ae511148a01ec0f75',
+    
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: false,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: false,
+  
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 3,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 0,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'default',
+  },
+
   USERS: [
     {
       // 想要发送的人的名字
@@ -23,7 +44,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oboOi6YVE7fL2bERhOaqSw-zBEwM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'wQA21lqTdQIZhks-LpYgdq4Z-z-rhwZvDojjLSq7s3Y',
+      useTemplateId: 'urf2tu8WKDmoCmpgHRmNzwAlbyd1KRjFaiT8qyNfX3E',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-21',
       festivals: [
