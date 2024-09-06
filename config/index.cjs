eddd8e932eaed5b616eb3a678635926aab9796bd
jack-nack <109973638+lcx777@users.noreply.github.com>
2024-09-06 23:21:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '瓜兮兮',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oboOi6YVE7fL2bERhOaqSw-zBEwM',
+      id: 'oboOi6VzVQtYV9yhJKNd5cLAToZs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'DyzYDg1Gz5mIBJwksVZ5TjlsmAjBU7yxQgbpb2uNCpk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
